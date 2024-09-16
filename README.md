ce65eebbe3a6dcadb667d7ef2dead8f191cd178e
tanat8350 <55195958+tanat8350@users.noreply.github.com>
2024-09-16 16:35:43 +0700

    update README, default qwerty, normal mode

--- README.md
+++ README.md
@@ -1,68 +1,78 @@
 # docs-vim
+
 ## Installation (Chrome)
+
 ```
 git clone https://github.com/matthewsot/docs-vim.git
 cd docs-vim
 git submodule init
 git submodule update
+git remote add upstream https://github.com/matthewsot/docs-vim.git
 ```
 
-* Go to `chrome://extensions/` in Chrome or Chromium.
-* Enable "Developer Mode". (Right top of the screen)
+- Go to `chrome://extensions/` in Chrome or Chromium.
+- Enable "Developer Mode". (Right top of the screen)
 
 ![image](https://user-images.githubusercontent.com/49868160/118260804-09b5ff00-b4b3-11eb-820a-58339ec95f33.png)
-* Click "Load unpacked". (Left top of the screen)
+
+- Click "Load unpacked". (Left top of the screen)
 
 ![image](https://user-images.githubusercontent.com/49868160/118261175-7f21cf80-b4b3-11eb-97b0-b2429a88b12c.png)
-* Select the docs-vim folder and click "Open"
+
+- Select the docs-vim folder and click "Open"
 
 ## Update
+
 Apparently Google is [making significant changes to the Docs
 editor](https://workspaceupdates.googleblog.com/2021/05/Google-Docs-Canvas-Based-Rendering-Update.html),
 so this will likely stop working in the next few months.
 
 ## Overview
+
 This simulates very limited versions of vim's insert and normal modes on Google
 Docs.
 
 Currently supported features:
-* Insert mode:
-    * Typing
-    * Escaping to normal with Esc
-    * Escaping to normal with a two-key combination
-* Normal mode
-    * Getting to insert mode with the "i" key
-    * Moving with the home-row keys
-    * Repeated motions using the number keys
-    * Delete a single character with the "x" key
-    * Enter visual mode with the "v" key
-* Visual mode
-    * Getting to normal mode with Esc
-    * Moving with the home-row keys
-    * Repeated motions using the number keys
-    * Delete selected text with the "x" key
-* Chrome (tested on 63.0)
-* (Not really) Firefox
-    * It will install, but many of the features work incorrectly.
-    
+
+- Insert mode:
+  - Typing
+  - Escaping to normal with Esc
+  - Escaping to normal with a two-key combination
+- Normal mode
+  - Getting to insert mode with the "i" key
+  - Moving with the home-row keys
+  - Repeated motions using the number keys
+  - Delete a single character with the "x" key
+  - Enter visual mode with the "v" key
+- Visual mode
+  - Getting to normal mode with Esc
+  - Moving with the home-row keys
+  - Repeated motions using the number keys
+  - Delete selected text with the "x" key
+- Chrome (tested on 63.0)
+- (Not really) Firefox
+  - It will install, but many of the features work incorrectly.
+
 ## How?
+
 Glad you asked - this project is built with the [docs-plus
 library](https://github.com/matthewsot/docs-plus), which enables rich
 interactions with the Google Docs editor. Improvements made to the library here
-(particularly those in the ``docs-plus-plus.js`` file) will probably eventually
+(particularly those in the `docs-plus-plus.js` file) will probably eventually
 be pushed back up to the main docs-plus library.
 
 ## Keys
+
 Currently set up to work with my DVORAK layout, these are the default keybindings:
 
-* Insert Mode:
-    * Esc: enter normal mode
-    * hn: enter normal mode
-* Normal Mode:
-    * i: enter insert mode
-    * dhtn: move
+- Insert Mode:
+  - Esc: enter normal mode
+  - hn: enter normal mode
+- Normal Mode:
+  - i: enter insert mode
+  - dhtn: move
 
-Note that most of these can be changed in ``docs-vim.js``. For example, if you
+Note that most of these can be changed in `docs-vim.js`. For example, if you
 want to use the more common QWERTY keybindings, you'd change to:
 
 ```
@@ -75,12 +85,13 @@ vim = {
 };
 ```
 
-Near the top of ``docs-vim.js``.
+Near the top of `docs-vim.js`.
 
 ## Future Features
+
 I don't really use all that much of vim's shortcuts, but a few particular things I want to add
 eventually:
 
-* Selections with shift-V (visual line mode)
-* Command mode with find and replace, go to line #, etc.
-* Quick find with /
+- Selections with shift-V (visual line mode)
+- Command mode with find and replace, go to line #, etc.
+- Quick find with /
--- docs-vim.js
+++ docs-vim.js
@@ -1,185 +1,194 @@
-const useQWERTY = false;
-const directionalKeys = useQWERTY ? "hjkl" : "dhtn";
+// default dvorak
+const useQWERTY = true;
+const directionalKeys = useQWERTY ? 'hjkl' : 'dhtn';
+
+// change initial cursor width
+docs.setCursorWidth('7px');
 
 vim = {
-    "mode": "insert", // Keep track of current mode
-    "num": "", // Keep track of number keys pressed by the user
-    "currentSequence": "", // Keep track of key sequences
-    "escapeSequence": useQWERTY ? "jk" : "hn",
-    "keyMaps" : {
-        "Backspace": [["ArrowLeft"]],
-        "x": [["Delete"]],
-        "b": [["ArrowLeft", true]], // ctrl + <-
-        "e": [["ArrowRight", true]], // ctrl + ->
-        // w is same behavior as eeb
-        "w": [["ArrowRight", true], ["ArrowRight", true], ["ArrowLeft", true]],
-        "a": [["ArrowRight"]],
-        "A": [["ArrowDown", true]],
-        "I": [["ArrowUp", true]],
-        "$": [["ArrowDown", true]],
-        "0": [["ArrowUp", true]],
-        "o": [["ArrowDown", true], ["Enter"]],
-        "O": [["ArrowUp", true], ["ArrowLeft"], ["Enter"]]
-    },
-    "needsInsert": ["a", "A", "I", "o", "O"]
+  // default 'insert'
+  mode: 'normal', // Keep track of current mode
+  num: '', // Keep track of number keys pressed by the user
+  currentSequence: '', // Keep track of key sequences
+  escapeSequence: useQWERTY ? 'jk' : 'hn',
+  keyMaps: {
+    Backspace: [['ArrowLeft']],
+    x: [['Delete']],
+    b: [['ArrowLeft', true]], // ctrl + <-
+    e: [['ArrowRight', true]], // ctrl + ->
+    // w is same behavior as eeb
+    w: [
+      ['ArrowRight', true],
+      ['ArrowRight', true],
+      ['ArrowLeft', true],
+    ],
+    a: [['ArrowRight']],
+    A: [['ArrowDown', true]],
+    I: [['ArrowUp', true]],
+    $: [['ArrowDown', true]],
+    0: [['ArrowUp', true]],
+    o: [['ArrowDown', true], ['Enter']],
+    O: [['ArrowUp', true], ['ArrowLeft'], ['Enter']],
+  },
+  needsInsert: ['a', 'A', 'I', 'o', 'O'],
 };
 
-vim.keyMaps[directionalKeys[0]] = [["ArrowLeft"]];
-vim.keyMaps[directionalKeys[1]] = [["ArrowDown"]];
-vim.keyMaps[directionalKeys[2]] = [["ArrowUp"]];
-vim.keyMaps[directionalKeys[3]] = [["ArrowRight"]];
+vim.keyMaps[directionalKeys[0]] = [['ArrowLeft']];
+vim.keyMaps[directionalKeys[1]] = [['ArrowDown']];
+vim.keyMaps[directionalKeys[2]] = [['ArrowUp']];
+vim.keyMaps[directionalKeys[3]] = [['ArrowRight']];
 
 vim.addKeyMappings = function (baseMap) {
-    baseMap[vim.keys.move[0]] = "ArrowLeft";
-    baseMap[vim.keys.move[1]] = "ArrowDown";
-    baseMap[vim.keys.move[2]] = "ArrowUp";
-    baseMap[vim.keys.move[3]] = "ArrowRight";
+  baseMap[vim.keys.move[0]] = 'ArrowLeft';
+  baseMap[vim.keys.move[1]] = 'ArrowDown';
+  baseMap[vim.keys.move[2]] = 'ArrowUp';
+  baseMap[vim.keys.move[3]] = 'ArrowRight';
 };
 
 vim.switchToNormalMode = function () {
-    vim.currentSequence = "";
-    vim.mode = "normal";
-    vim.num = "";
-    docs.setCursorWidth("7px");
+  vim.currentSequence = '';
+  vim.mode = 'normal';
+  vim.num = '';
+  docs.setCursorWidth('7px');
 };
 
 vim.switchToVisualMode = function () {
-    vim.currentSequence = "";
-    vim.mode = "visual";
-    vim.num = "";
-    docs.setCursorWidth("7px");
+  vim.currentSequence = '';
+  vim.mode = 'visual';
+  vim.num = '';
+  docs.setCursorWidth('7px');
 };
 
 vim.switchToInsertMode = function () {
-    vim.currentSequence = "";
-    vim.mode = "insert";
-    vim.num = "";
-    docs.setCursorWidth("2px");
+  vim.currentSequence = '';
+  vim.mode = 'insert';
+  vim.num = '';
+  docs.setCursorWidth('2px');
 };
 
 // Called in normal mode.
 vim.normal_keydown = function (e) {
-    if (e.key.match(/F\d+/)) {
-        // Pass through any function keys.
-        return true;
+  if (e.key.match(/F\d+/)) {
+    // Pass through any function keys.
+    return true;
+  }
+
+  e.preventDefault();
+  e.stopPropagation();
+
+  if (e.key == 'i') {
+    vim.switchToInsertMode();
+    return true;
+  }
+
+  if (e.key == 'v') {
+    vim.switchToVisualMode();
+    return true;
+  }
+
+  if (e.key.match(/\d+/)) {
+    vim.num += e.key.toString();
+  }
+
+  vim.keyMaps[e.key]?.forEach(([key, ...args]) => {
+    const numRepeats = parseInt(vim.num) || 1;
+    for (let i = 0; i < numRepeats; i++) {
+      docs.pressKey(docs.codeFromKey(key), ...args);
     }
+    vim.num = '';
+  });
 
-    e.preventDefault();
-    e.stopPropagation();
+  if (vim.needsInsert.includes(e.key)) {
+    vim.switchToInsertMode();
+    return true;
+  }
 
-    if (e.key == "i") {
-        vim.switchToInsertMode();
-        return true;
-    }
-
-    if (e.key == "v") {
-        vim.switchToVisualMode();
-        return true;
-    }
-
-    if (e.key.match(/\d+/)) {
-        vim.num += e.key.toString();
-    }
-
-    vim.keyMaps[e.key]?.forEach(([key, ...args]) => {
-        const numRepeats = parseInt(vim.num) || 1;
-        for (let i = 0; i < numRepeats; i++) {
-            docs.pressKey(docs.codeFromKey(key), ...args);
-        }
-        vim.num = "";
-    });
-
-    if (vim.needsInsert.includes(e.key)) {
-        vim.switchToInsertMode();
-        return true;
-    }
-
-    return false;
+  return false;
 };
 
 // Called in visual mode.
 vim.visual_keydown = function (e) {
-    if (e.key.match(/F\d+/)) {
-        // Pass through any function keys.
-        return true;
-    }
+  if (e.key.match(/F\d+/)) {
+    // Pass through any function keys.
+    return true;
+  }
 
-    if (e.key == "Escape") {
-        // Escape visual mode.
-        vim.switchToNormalMode();
-    }
+  if (e.key == 'Escape') {
+    // Escape visual mode.
+    vim.switchToNormalMode();
+  }
 
-    vim.currentSequence += e.key;
-    if (vim.currentSequence == vim.escapeSequence) {
-        e.preventDefault();
-        e.stopPropagation();
-
-        vim.switchToNormalMode();
-        return false;
-    }
-    if (vim.escapeSequence.indexOf(vim.currentSequence) != 0) {
-        vim.currentSequence = e.key;
-    }
-
+  vim.currentSequence += e.key;
+  if (vim.currentSequence == vim.escapeSequence) {
     e.preventDefault();
     e.stopPropagation();
 
-    if (e.key.match(/\d+/)) {
-        vim.num += e.key.toString();
-    }
-
-    vim.keyMaps[e.key]?.forEach(([key, ...args]) => {
-        const numRepeats = parseInt(vim.num) || 1;
-        for (let i = 0; i < numRepeats; i++) {
-            if (key.indexOf("Arrow") == 0) {
-                // get the special keys pressed and default to false
-                const keyArgs = [...args, false, false].slice(0, 2);
-                keyArgs[1] = true;
-                docs.pressKey(docs.codeFromKey(key), ...keyArgs);
-            } else {
-                docs.pressKey(docs.codeFromKey(key), ...args);
-                vim.switchToNormalMode();
-            }
-        }
-        vim.num = "";
-    });
-
+    vim.switchToNormalMode();
     return false;
+  }
+  if (vim.escapeSequence.indexOf(vim.currentSequence) != 0) {
+    vim.currentSequence = e.key;
+  }
+
+  e.preventDefault();
+  e.stopPropagation();
+
+  if (e.key.match(/\d+/)) {
+    vim.num += e.key.toString();
+  }
+
+  vim.keyMaps[e.key]?.forEach(([key, ...args]) => {
+    const numRepeats = parseInt(vim.num) || 1;
+    for (let i = 0; i < numRepeats; i++) {
+      if (key.indexOf('Arrow') == 0) {
+        // get the special keys pressed and default to false
+        const keyArgs = [...args, false, false].slice(0, 2);
+        keyArgs[1] = true;
+        docs.pressKey(docs.codeFromKey(key), ...keyArgs);
+      } else {
+        docs.pressKey(docs.codeFromKey(key), ...args);
+        vim.switchToNormalMode();
+      }
+    }
+    vim.num = '';
+  });
+
+  return false;
 };
 
 // Called in insert mode.
 vim.insert_keydown = function (e) {
-    if (e.key == "Escape") {
-        vim.switchToNormalMode();
+  if (e.key == 'Escape') {
+    vim.switchToNormalMode();
+  }
+
+  vim.currentSequence += e.key;
+  if (vim.currentSequence == vim.escapeSequence) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    // We need to delete the first character already typed in the escape
+    // sequence.
+    for (var i = 0; i < vim.currentSequence.length - 1; i++) {
+      docs.backspace();
     }
 
-    vim.currentSequence += e.key;
-    if (vim.currentSequence == vim.escapeSequence) {
-        e.preventDefault();
-        e.stopPropagation();
-
-        // We need to delete the first character already typed in the escape
-        // sequence.
-        for (var i = 0; i < (vim.currentSequence.length - 1); i++) {
-            docs.backspace();
-        }
-
-        vim.switchToNormalMode();
-        return false;
-    }
-    if (vim.escapeSequence.indexOf(vim.currentSequence) != 0) {
-        vim.currentSequence = e.key;
-    }
+    vim.switchToNormalMode();
+    return false;
+  }
+  if (vim.escapeSequence.indexOf(vim.currentSequence) != 0) {
+    vim.currentSequence = e.key;
+  }
 };
 
 docs.keydown = function (e) {
-    if (vim.mode == "insert") {
-        return vim.insert_keydown(e);
-    }
-    if (vim.mode == "normal") {
-        return vim.normal_keydown(e);
-    }
-    if (vim.mode == "visual") {
-        return vim.visual_keydown(e);
-    }
+  if (vim.mode == 'insert') {
+    return vim.insert_keydown(e);
+  }
+  if (vim.mode == 'normal') {
+    return vim.normal_keydown(e);
+  }
+  if (vim.mode == 'visual') {
+    return vim.visual_keydown(e);
+  }
 };
